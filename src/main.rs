7ce5a4c1fcededecc2706f2f73f73f8b1379107a
Brian Thuo <thuogachau@gmail.com>
2025-07-17 14:40:22 +0300

    Refactor file handling in main.rs to improve error handling and update file path constant

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+const FILE_EXTENSION: &str = "/Users/brianthuo/Documents/GitHub/FileIOUtils/input_text.rtf";
+
 fn main() {
-
     //file path:
-    let file_path: &str = "src/input.rtf";
-    
-    let mut f: File = File::open(file_path).expect("Could not read the .txt file");
+    let file_path: &str = FILE_EXTENSION;
+
+    let mut f = match File::open(file_path) {
+        Ok(file) => {
+            println!("File opened successfully.");
+            file
+        }
+        Err(e) => {
+            match e.kind() {
+                std::io::ErrorKind::NotFound => {
+                    eprintln!("File not found: {}", file_path);
+                }
+                std::io::ErrorKind::PermissionDenied => {
+                    eprintln!("Permission denied: {}", file_path);
+                }
+                _ => eprintln!("An error occurred while opening the file: {}", e),
+            }
+            return;
+        }
+    };
 
     // Extract and print the text content
-    let mut text_content:String  = String::new();
+    let mut text_content: String = String::new();
 
-    // Extract and print the text content
-    f.read_to_string(&mut text_content).expect("Could not extract text from the .txt file");
+    f.read_to_string(&mut text_content)
+        .expect("Could not extract text from the .txt file");
     println!("\nThe file contents as below:: \n\n{}", text_content);
 }
