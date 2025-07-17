61f110d6aadef338361dd953042a9015771312c2
Brian Thuo <thuogachau@gmail.com>
2025-07-17 14:21:21 +0300

    Implement file reading functionality and add input_text.rtf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    //file path:
+    let file_path: &str = "src/input.rtf";
+    
+    let mut f: File = File::open(file_path).expect("Could not read the .txt file");
+
+    // Extract and print the text content
+    let mut text_content:String  = String::new();
+
+    // Extract and print the text content
+    f.read_to_string(&mut text_content).expect("Could not extract text from the .txt file");
+    println!("\nThe file contents as below:: \n\n{}", text_content);
 }
